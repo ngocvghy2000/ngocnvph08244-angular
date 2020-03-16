db536b186edcb4df40f32110e41c80d729ec633e
ngocvghy2000 <ngocvghy2000@gmail.com>
2020-03-16 17:33:59 +0700

    4 files modified, 13 added, 26 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SlideComponent } from './slide/slide.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, ProductComponent, ProductListComponent, CategoryComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
