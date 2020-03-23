067b58d57ed62ea3d15357d152953abfa0b4cc90
ngocvghy2000 <ngocvghy2000@gmail.com>
2020-03-23 14:55:27 +0700

    1 file modified, 1 added

---
+++ src/app/product-service/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { Sanpham } from "../sanpham";
+import { Data } from "../data";
+@Injectable()
+export class ProductService {
+  products = Data;
+  constructor() {}
+
+  getListProduct() {
+    return this.products;
+  }
+  addProduct(sp) {
+    let newObj = { id: 6, ...sp };
+    this.products.push(newObj);
+  }
+}
