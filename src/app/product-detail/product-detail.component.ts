e27250c47e6658aded6a223df62fc637f779ee45
ngocvghy2000 <ngocvghy2000@gmail.com>
2020-03-25 23:49:49 +0700

    2 files modified

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Sanpham } from "../sanpham";
-import { Data } from "../data";
+// import { Data } from "../data";
 import { ProductService } from "../product-service/product.service";
 import { ActivatedRoute } from "@angular/router";
 
