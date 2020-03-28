15249a86c6047ab9414a8f541593c7eecacd52b4
ngocvghy2000 <ngocvghy2000@gmail.com>
2020-03-28 15:22:24 +0700

    1 file modified

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,27 +1,51 @@
 import { Sanpham } from "./sanpham";
 export const Data: Sanpham[] = [
-  {
+ {
     id: 1,
-    name: "sữa",
-    price: 456,
-    description: "mô tả sản phẩm 1",
-    status: true,
-    img: "",
+    name: "Sugar",
+    price: 74.00,
+    description: "description 1",
+    img: "https://www.upsieutoc.com/images/2020/03/28/pic.jpg",
+    status: false
   },
   {
     id: 2,
-    name: "bánh",
-    price: 123,
-    description: "mô tả sản phẩm 2",
-    status: true,
-    img: "",
+    name: "Table",
+    price: 98.00,
+    description: "description 2",
+    img: "https://www.upsieutoc.com/images/2020/03/28/pic1.jpg",
+    status: false
   },
   {
     id: 3,
-    name: "kẹo",
-    price: 999,
-    description: "mô tả sản phẩm 3",
-    status: true,
-    img: "",
+    name: "Computer",
+    price : 37.00,
+    descriptio: "description 3",
+    img: "https://www.upsieutoc.com/images/2020/03/28/pic2.jpg",
+    status: false
+  },
+  {
+    id : 4,
+    name: "Computer",
+    price : 261.00,
+    description: "description 4",
+    img: "https://www.upsieutoc.com/images/2020/03/28/pic3.jpg",
+    status: false
+  },
+  {
+    id: 5,
+    name: "Tuna",
+    price: 393.00,
+    description: "description 5",
+    img: "https://www.upsieutoc.com/images/2020/03/28/pic4.jpg",
+    status: false
+  },
+  {
+    id: 6,
+    name: "Chips",
+    price: 86.00,
+    description: "description 6",
+    img: "https://www.upsieutoc.com/images/2020/03/28/pic5.jpg",
+    status: true
   }
 ];
